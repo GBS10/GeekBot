16053fa179867ef0b352207c0782495c2ffe4a79
SubikshaGB <48596109+GBS10@users.noreply.github.com>
2022-01-31 10:00:27 +0530

    Update db.py

--- flask/db.py
+++ flask/db.py
@@ -2,8 +2,11 @@ from flask import Flask, request, jsonify, make_response
 from flaskext.mysql import MySQL
 import pymysql 
 import flask
+
+#Creating flask instance
 app = Flask(__name__)
 
+#Initiating MySQL
 mysql = MySQL()
    
 # MySQL configurations
@@ -13,15 +16,15 @@ app.config['MYSQL_DATABASE_DB'] = 'demo'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-
+#Function to enter a new word and its definition into te WORDDEF table
 @app.route('/add/<word>/<definition>', methods =['POST'])
 def register(word, definition):
         w = word 
         d = definition
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('INSERT INTO WORDDEF VALUES(%s, %s)', (w, d))
-        conn.commit()
+        conn = mysql.connect() #Establishing connection with MySQL server
+        cursor = conn.cursor(pymysql.cursors.DictCursor) #Creating a cursor
+        cursor.execute('INSERT INTO WORDDEF VALUES(%s, %s)', (w, d)) #Executing insert command
+        conn.commit() #Commiting the changes to end the transaction
         print("Connected")
         return flask.Response(status=200)
 if __name__ == '__main__':
